48612d2864269d461c1afbf6a7aa0faca68658f9
Nathaniel Brion Sison <ngsison@outlook.com>
2018-08-14 15:17:34 +0800

    Added image assets; Finished ep1

--- instaclone/ViewController.swift
+++ instaclone/ViewController.swift
@@ -10,16 +10,127 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    // MARK: PROPERTIES
+    let plusButton: UIButton = {
+        let button = UIButton(type: UIButtonType.system)
+        button.setImage(#imageLiteral(resourceName: "plus_photo").withRenderingMode(UIImageRenderingMode.alwaysOriginal), for: UIControlState.normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    let emailTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Email"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = UITextBorderStyle.roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let usernameTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Username"
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = UITextBorderStyle.roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.placeholder = "Password"
+        tf.isSecureTextEntry = true
+        tf.backgroundColor = UIColor(white: 0, alpha: 0.03)
+        tf.borderStyle = UITextBorderStyle.roundedRect
+        tf.font = UIFont.systemFont(ofSize: 14)
+        tf.translatesAutoresizingMaskIntoConstraints = false
+        return tf
+    }()
+    
+    let signUpButton: UIButton = {
+        let button = UIButton(type: UIButtonType.system)
+        button.setTitle("Sign Up", for: UIControlState.normal)
+        button.backgroundColor = UIColor(red: 149/255, green: 204/255, blue: 244/255, alpha: 1)
+        button.layer.cornerRadius = 5
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
+        button.setTitleColor(UIColor.white, for: UIControlState.normal)
+        return button
+    }()
+    
+    
+    
+    // MARK: OVERRIDES
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        setupViews()
     }
 
 
 }
+
+
+
+// MARK: EXTENSION - SETUP VIEWS
+extension ViewController {
+    
+    func setupViews() {
+        setupPlusButton()
+        setupInputFields()
+    }
+    
+    private func setupPlusButton() {
+        self.view.addSubview(plusButton)
+        
+        NSLayoutConstraint.activate([
+            plusButton.heightAnchor.constraint(equalToConstant: 140),
+            plusButton.widthAnchor.constraint(equalToConstant: 140),
+            plusButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            plusButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 40)
+        ])
+    }
+    
+    private func setupInputFields() {
+        
+        let stackView = UIStackView()
+        stackView.distribution = UIStackViewDistribution.fillEqually
+        stackView.axis = UILayoutConstraintAxis.vertical
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(emailTextField)
+        stackView.addArrangedSubview(usernameTextField)
+        stackView.addArrangedSubview(passwordTextField)
+        stackView.addArrangedSubview(signUpButton)
+        
+        self.view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.topAnchor.constraint(equalTo: plusButton.bottomAnchor, constant: 20),
+            stackView.leftAnchor.constraint(equalTo: self.view.leftAnchor, constant: 40),
+            stackView.rightAnchor.constraint(equalTo: self.view.rightAnchor, constant: -40),
+            stackView.heightAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
